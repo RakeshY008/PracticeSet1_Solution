37b3e61232f43738cdb98e7d3a397e2e293a8052
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-08-23 21:32:17 +0530

    INit 1 Commit

---
+++ Practice Questions Set 1/src/PracticeSet1/Question1.java
@@ -0,0 +1,21 @@
+
+package PracticeSet1;
+/*
+    @ Author: Rakesh Yadav
+    @ Version: 12.0.2
+ */
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        String nobita = "University";
+        Scanner scanner = new Scanner(System.in);
+        String shizuka1 = scanner.next();
+        String shizuka = scanner.next();
+
+        System.out.println(shizuka1 + " " + nobita + " " + shizuka);
+
+        scanner.close();
+    }
+}
